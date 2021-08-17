65d1c26ec8e655886a9bc072aa4eb9b4a87b8759
Kornel David <konel1020@gmail.com>
2021-08-17 12:50:21 +0200

    LDS Add functions for creating Envoy listeners

--- controlplane/discoveryservices/lds.go
+++ controlplane/discoveryservices/lds.go
@@ -2,9 +2,16 @@ package discoveryservices
 
 import (
 	"context"
+	l7mpiov1 "github.com/davidkornel/operator/api/v1"
+	"github.com/davidkornel/operator/state"
 	pb "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	udp "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/udp/udp_proxy/v3"
 	envoyservicediscoveryv3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	listenerservice "github.com/envoyproxy/go-control-plane/envoy/service/listener/v3"
+	"github.com/golang/protobuf/ptypes/any"
+	"google.golang.org/protobuf/types/known/anypb"
 	"io"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
@@ -35,7 +42,44 @@ func (s *listenerDiscoveryService) DeltaListeners(server listenerservice.Listene
 			return err
 		}
 		logger.Info("Request:", "Node-id: ", ddr.Node.Id)
+		if _, ok := state.LdsChannels[ddr.Node.Id]; !ok {
+			state.LdsChannels[ddr.Node.Id] = make(chan []*listener.Listener)
+			logger.Info("Channel has been added for", "uid", ddr.Node.Id)
+		}
+		listeners := <-state.LdsChannels[ddr.Node.Id]
+		logger.Info("listeners", "l", listeners)
+		err = server.Send(CreateDeltaDiscoveryResponse(listeners))
+		if err != nil {
+			return err
+		}
 	}
+}
+
+func CreateDeltaDiscoveryResponse(listeners []*listener.Listener) *envoyservicediscoveryv3.DeltaDiscoveryResponse {
+	var resources []*envoyservicediscoveryv3.Resource
+	for _, l := range listeners {
+		r := &envoyservicediscoveryv3.Resource{
+			Name:     l.Name,
+			Version:  "1",
+			Resource: ConvertListenersToAny(l),
+		}
+		resources = append(resources, r)
+	}
+
+	response := envoyservicediscoveryv3.DeltaDiscoveryResponse{
+		SystemVersionInfo: "Testversion",
+		Resources:         resources,
+		TypeUrl:           "type.googleapis.com/envoy.config.listener.v3.Listener",
+		RemovedResources:  nil,
+		Nonce:             "listener",
+	}
+
+	return &response
+}
+
+func ConvertListenersToAny(l *listener.Listener) *any.Any {
+	listenerAny, _ := anypb.New(l)
+	return listenerAny
 }
 
 func NewServer() *listenerDiscoveryService {
@@ -45,4 +89,53 @@ func NewServer() *listenerDiscoveryService {
 	logger := ctrl.Log.WithName("newserver")
 	logger.Info("newserver init")
 	return s
+}
+
+func CreateEnvoyListenerConfigFromVsvcSpec(spec l7mpiov1.VirtualServiceSpec) []*listener.Listener {
+	var listeners []*listener.Listener
+	// TODO handle tcp listener aswell
+	for _, l := range spec.Listeners {
+		udpFilter := &udp.UdpProxyConfig{
+			StatPrefix: l.Name,
+			RouteSpecifier: &udp.UdpProxyConfig_Cluster{
+				Cluster: l.Udp.Cluster.Name,
+			},
+			HashPolicies: []*udp.UdpProxyConfig_HashPolicy{{
+				PolicySpecifier: &udp.UdpProxyConfig_HashPolicy_Key{
+					Key: l.Udp.Cluster.HashKey,
+				},
+			},
+			},
+		}
+
+		//pbst, err := ptypes.MarshalAny(udpFilter)
+		pbst, err := anypb.New(udpFilter)
+		if err != nil {
+			panic(err)
+		}
+
+		list := &listener.Listener{
+			Name:      l.Name,
+			ReusePort: true,
+			Address: &core.Address{
+				Address: &core.Address_SocketAddress{
+					SocketAddress: &core.SocketAddress{
+						Protocol: core.SocketAddress_UDP,
+						Address:  "0.0.0.0",
+						PortSpecifier: &core.SocketAddress_PortValue{
+							PortValue: l.Udp.Port,
+						},
+					},
+				},
+			},
+			ListenerFilters: []*listener.ListenerFilter{{
+				Name: "envoy.filters.udp_listener.udp_proxy",
+				ConfigType: &listener.ListenerFilter_TypedConfig{
+					TypedConfig: pbst,
+				},
+			}},
+		}
+		listeners = append(listeners, list)
+	}
+	return listeners
 }
