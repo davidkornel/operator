935c3549aba8672da9afa1f97dbd512b73708cf6
Kornel David <konel1020@gmail.com>
2021-11-30 18:17:58 +0100

    Handle pod deletion better

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -19,15 +19,19 @@ package controllers
 import (
 	"context"
 	"github.com/davidkornel/operator/state"
+	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	"time"
 )
 
-const labelKeys = "app"
+const (
+	labelKeys = "app"
+)
 
 var (
 	//The pod labelValues that we are interested in
@@ -54,8 +58,7 @@ type PodReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := log.FromContext(ctx)
-
+	logger := ctrl.Log.WithName("POD_C")
 	// your logic here
 	var pod corev1.Pod
 	if err := r.Get(ctx, req.NamespacedName, &pod); err != nil {
@@ -68,22 +71,36 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	}
 	isPodMarkedToBeDeleted := pod.GetDeletionTimestamp() != nil
 	if isPodMarkedToBeDeleted {
-		for i, p := range state.ClusterState.Pods {
-			if p.UID == pod.UID {
-				state.ClusterState.Pods = remove(state.ClusterState.Pods, i)
-				close(state.LdsChannels[string(pod.UID)])
-				close(state.CdsChannels[string(pod.UID)])
-				//logger.Info("Removed pod from Pods", "uid", pod.UID)
-				//logger.Info("Channels closed", "uid", pod.UID)
-				return ctrl.Result{}, nil
+		//logger.Info("pod marked to be deleted")
+		//TODO this is a huge mess but works, reduce loops and 'if's
+		for _, a := range pod.Status.ContainerStatuses {
+			//TODO with graceperiod set to 0 or 1 container won't reach the terminated state
+			if a.Name == "envoy" /*&& a.State.Terminated != nil*/ {
+				for i, p := range state.ClusterState.Pods {
+					if p.UID == pod.UID {
+						state.ClusterState.Pods = remove(state.ClusterState.Pods, i)
+
+						delete(state.LdsChannels, string(pod.UID))
+						delete(state.LdsChannels, string(pod.UID))
+						go r.CloseChannels(logger, ctx, pod)
+						logger.Info("Removed pod from Pods", "pod", pod.Name, "uid", pod.UID)
+						logger.Info("Number of elements in Pods", "", len(state.ClusterState.Pods))
+						state.PodChannel <- state.SignalMessageOnPodChannel{
+							Verb: 1,
+							Pod:  &pod,
+						}
+						return ctrl.Result{}, nil
+					}
+				}
 			}
 		}
-		//logger.Info("Pod already removed from Pods", "uid", pod.UID)
+
 		return ctrl.Result{}, nil
 	}
 	for _, label := range labelValues {
 		labelIsPresent := pod.Labels[labelKeys] == label
 		if labelIsPresent {
+			//TODO should we wait till the pod is in phase running or not???
 			if pod.Status.Phase == "Running" && pod.Status.PodIP != "" {
 				for i, p := range state.ClusterState.Pods {
 					if p.UID == pod.UID {
@@ -93,11 +110,44 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 					}
 				}
 				state.ClusterState.Pods = append(state.ClusterState.Pods, pod)
-				//logger.Info("pod added to Pods:", "name: ", pod.Name, " pod.uid: ", pod.UID)
+				state.PodChannel <- state.SignalMessageOnPodChannel{
+					Verb: 0,
+					Pod:  &pod,
+				}
+
+				logger.Info("Pod added to Pods:", "name: ", pod.Name, " pod.uid: ", pod.UID)
 			}
 		}
 	}
 	return ctrl.Result{}, nil
+}
+
+func (r *PodReconciler) CloseChannels(logger logr.Logger, ctx context.Context, pod corev1.Pod) {
+	for {
+		p := corev1.Pod{}
+		if err := r.Get(ctx, types.NamespacedName{
+			Namespace: pod.Namespace,
+			Name:      pod.Name,
+		}, &p); err != nil {
+			if apierrors.IsNotFound(err) {
+				logger.Info("Pod not found, closing its channels")
+				if _, ok := state.LdsChannels[string(pod.UID)]; ok {
+					close(state.LdsChannels[string(pod.UID)])
+				}
+				if _, ok := state.CdsChannels[string(pod.UID)]; ok {
+					close(state.CdsChannels[string(pod.UID)])
+				}
+				for _, e := range state.ConnectedEdsClients {
+					if e.UID == string(pod.UID) {
+						close(e.Channel)
+						logger.Info("Closing eds channel")
+					}
+				}
+				return
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
